d344237d99d2e607fd5bfafda1293713c4c04101
Anton Kabanov <imcreativchik@gmail.com>
2023-08-28 12:35:58 +0500

    add model for Follow, Group

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,6 +3,38 @@ from django.db import models
 
 User = get_user_model()
 
+RESTRICTION_POST_CHARACTERS = 30
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Подписан',
+    )
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор'
+    )
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.title[:RESTRICTION_POST_CHARACTERS]
+
 
 class Post(models.Model):
     author = models.ForeignKey(
@@ -22,21 +54,16 @@ class Post(models.Model):
         verbose_name='Изображение',
     )
     group = models.ForeignKey(
-        Groups,
+        Group,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='posts',
-        verbose_name='Сообщество',
+        verbose_name='Группа',
     )
 
-    class Meta:
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Посты'
-        ordering = ('id',)
-
     def __str__(self):
-        return self.text
+        return self.text[:RESTRICTION_POST_CHARACTERS]
 
 
 class Comment(models.Model):
