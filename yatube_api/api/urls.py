0271540ef2f41cf5619bae9778f6b44d68e43092
Anton Kabanov <imcreativchik@gmail.com>
2023-08-28 13:13:56 +0500

    refactor urls for djoser

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -16,6 +16,6 @@ v1_router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
