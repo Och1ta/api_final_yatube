3108ca1ffbffd5845f05c2f710aeed7632ae4af8
Anton Kabanov <imcreativchik@gmail.com>
2023-08-29 11:48:10 +0500

    add check validate in FollowSerializer

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Follow, Group, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -42,11 +43,30 @@ class GroupSerializer(serializers.ModelSerializer):
 class FollowSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Follow"""
 
-    user = serializers.SlugRelatedField(
+    user = SlugRelatedField(
+        slug_field='username',
         read_only=True,
-        slug_field='username'
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
     )
 
     class Meta:
         fields = '__all__'
         model = Follow
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=("Вы уже подписаны на автора")
+            ),
+        )
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на себя!'
+            )
+        return data
