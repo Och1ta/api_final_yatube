e690ed2cf3221b2359c8e6657808391188fb9e34
Anton Kabanov <imcreativchik@gmail.com>
2023-08-31 11:41:20 +0500

    del unused permission

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -7,12 +7,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     только владельцам.
     """
 
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
